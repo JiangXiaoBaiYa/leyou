1890003e42a99ea463177abe40e3fdf27fc50caf
JiangGuangming <735825678@qq.com>
2019-05-20 22:20:39 +0800

    订单微服务Mapper的补充

---
+++ ly-order/src/main/java/com/leyou/order/mapper/OrderDetailMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.order.mapper;
+
+import com.leyou.order.entity.OrderDetail;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author: 姜光明
+ * @Date: 2019/5/20 22:18
+ */
+public interface OrderDetailMapper extends Mapper<OrderDetail> {
+}
---
+++ ly-order/src/main/java/com/leyou/order/mapper/OrderLogisticsMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.order.mapper;
+
+import com.leyou.order.entity.OrderLogistics;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author: 姜光明
+ * @Date: 2019/5/20 22:19
+ */
+public interface OrderLogisticsMapper extends Mapper<OrderLogistics> {
+}
