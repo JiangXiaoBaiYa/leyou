4359f67b8e0dd57f70ee7f6a51da2a032c81211b
JiangGuangming <735825678@qq.com>
2019-05-20 11:31:49 +0800

    修改购物车中商品数量

--- ly-cart/src/main/java/com/leyou/cart/controller/CartController.java
+++ ly-cart/src/main/java/com/leyou/cart/controller/CartController.java
@@ -5,9 +5,9 @@ import com.leyou.cart.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Author: 姜光明
@@ -30,4 +30,23 @@ public class CartController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+     * 查询购物车列表
+     * @return
+     */
+    @GetMapping("list")
+    public ResponseEntity<List<Cart>> queryCartList() {
+        List<Cart> carts = cartService.queryCartList();
+        if (carts == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(carts);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateNum(@RequestParam("id") Long skuId,
+                                          @RequestParam("num") Integer num) {
+        cartService.updateNum(skuId, num);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- ly-cart/src/main/java/com/leyou/cart/service/CartService.java
+++ ly-cart/src/main/java/com/leyou/cart/service/CartService.java
@@ -1,17 +1,24 @@
 package com.leyou.cart.service;
 
 import com.leyou.cart.entity.Cart;
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exceptions.LyException;
 import com.leyou.common.threadlocals.UserHolder;
 import com.leyou.common.utils.JsonUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @Author: 姜光明
  * @Date: 2019/5/20 10:07
  */
+@Slf4j
 @Service
 public class CartService {
     @Autowired
@@ -39,5 +46,50 @@ public class CartService {
         }
         //写入redis
         hashOps.put(hashKey,JsonUtils.toString(cart));
+    }
+
+    public List<Cart> queryCartList() {
+        //获取登录用户
+        String key = KEY_PREFIX + UserHolder.getUser();
+        //判断是否存在购物车
+        Boolean boo = redisTemplate.hasKey(key);
+        if (boo == null || !boo) {
+            //不存在，直接抛出异常
+            throw new LyException(ExceptionEnum.CARTS_NOT_FOUND);
+        }
+        BoundHashOperations<String, String, String> hashOps = redisTemplate.boundHashOps(key);
+
+        //判断是否有数据
+        Long size = hashOps.size();
+        if (size == null || size < 0) {
+            //不存在，直接返回
+            throw new LyException(ExceptionEnum.CARTS_NOT_FOUND);
+        }
+
+        List<String> carts = hashOps.values();
+        return carts.stream()
+                .map(json -> JsonUtils.toBean(json, Cart.class))
+                .collect(Collectors.toList());
+    }
+
+    public void updateNum(Long skuId, Integer num) {
+        //获取当前用户
+        Long uid = UserHolder.getUser();
+        String key = KEY_PREFIX + uid;
+        //获取hash操作的对象
+        BoundHashOperations<String, String, String> hashOps = redisTemplate.boundHashOps(key);
+        String hashKey = skuId.toString();
+        Boolean boo = hashOps.hasKey(hashKey);
+        if (boo == null || !boo) {
+            log.error("购物车商品不存在，用户：{}, 商品：{}", uid, skuId);
+            throw new LyException(ExceptionEnum.CARTS_NOT_FOUND);
+        }
+        //查询购物车商品
+        Cart c = JsonUtils.toBean(hashOps.get(hashKey), Cart.class);
+
+        //修改数量
+        c.setNum(num);
+        //写回redis
+        hashOps.put(hashKey, JsonUtils.toString(c));
     }
 }
